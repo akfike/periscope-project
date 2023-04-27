723cb3fc7ed38f52572e733f5140f8139740961e
Anne Katherine Fike <annekatherine.fike@adtran.com>
2023-04-27 10:30:05 -0500

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'periscope-project';
+// }
 import { Component } from '@angular/core';
+
+interface Entry {
+  name: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +21,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'periscope-project';
+  entries: Entry[] = [];
+  newEntry: Entry = { name: '', status: '' };
+
+  onSubmit(): void {
+    this.entries.push(this.newEntry);
+    this.newEntry = { name: '', status: '' };
+  }
+
+  delete(entry: Entry): void {
+    const index = this.entries.indexOf(entry);
+    if (index >= 0) {
+      this.entries.splice(index, 1);
+    }
+  }
+
+  deleteAll(): void {
+    this.entries = [];
+  }
 }
