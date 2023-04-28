8d69df8d08ab7818e15d17607c1e06ce94a31ee0
Sybersid <sybersid18@gmail.com>
2023-04-28 09:56:05 -0500

    colors

--- src/app/build.ts
+++ src/app/build.ts
@@ -1,4 +1,4 @@
 export interface Build {
     name: string,
-    status: "PASSED" | "FAILED" | "PAUSE"
+    status: "PASSED" | "FAILED" | "IN PROGRESS"
 }
--- src/app/mock-builds.ts
+++ src/app/mock-builds.ts
@@ -3,12 +3,12 @@ import { Build } from "./build";
 export const BUILDS: Build[] = [
     { name: "Jenkins Build 1", status: "PASSED"},
     { name: "Jenkins Build 2", status: "FAILED"},
-    { name: "Jenkins Build 3", status: "PAUSE"},
+    { name: "Jenkins Build 3", status: "IN PROGRESS"},
     { name: "Jenkins Build 4", status: "PASSED"},
     { name: "Jenkins Build 5", status: "PASSED"},
     { name: "Jenkins Build 6", status: "FAILED"},
     { name: "Jenkins Build 7", status: "PASSED"},
-    { name: "Jenkins Build 8", status: "PAUSE"},
+    { name: "Jenkins Build 8", status: "IN PROGRESS"},
     { name: "Jenkins Build 9", status: "PASSED"},
-    { name: "Jenkins Build 10", status: "PAUSE"}
+    { name: "Jenkins Build 10", status: "IN PROGRESS"}
 ];
